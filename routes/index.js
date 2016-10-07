5c34b0bad73af3089ad4071defc78d712b71f435
Ed Branch <Ed@MacBook-Pro.local>
2016-10-07 21:17:35 +0100

    Tidied source code

--- routes/index.js
+++ routes/index.js
@@ -1,46 +1,50 @@
-var express = require('express'),
-    router = express.Router(),
-    mongoose = require('mongoose'),
-    bodyParser = require('body-parser'),
-    methodOverride = require('method-override');
+var express = require('express')
+var router = express.Router()
+var mongoose = require('mongoose')
+var bodyParser = require('body-parser')
+var methodOverride = require('method-override')
+var contact = mongoose.model('contact')
+var organisation = mongoose.model('organisation')
+var opportunity = mongoose.model('opportunity')
 
-contact = mongoose.model('contact');
-organisation = mongoose.model('organisation');
-opportunity = mongoose.model('opportunity');
-
-router.use(bodyParser.urlencoded({ extended: true }));
-router.use(methodOverride(function(req, res){
-      if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-        // look in urlencoded POST bodies and delete it
-        var method = req.body._method;
-        delete req.body._method;
-        return method;
-      }
-}));
+router.use(bodyParser.urlencoded({ extended: true }))
+router.use(methodOverride(function (req, res) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    var method = req.body._method
+    delete req.body._method
+    return method
+  }
+}))
 
 router.route('/')
-    //GET all contacts
-    .get(function(req, res, next) {
-        //retrieve all contacts from Monogo
-        contact.find({}, function (err, contacts) {
+    .get(function (req, res, next) {
+      contact.find({}, function (err, contacts) {
+        if (err) {
+          return console.error(err)
+        } else {
           organisation.find({}, function (err, organisations) {
-            opportunity.find({}, function (err, opportunities) {
-              if (err) {
-                  return console.error(err);
-              } else {
-                    res.format({
-                    html: function(){
-                        res.render('index', {
-                              "contacts" : contacts,
-                              "organisations" : organisations,
-                              "opportunities" : opportunities
-                          });
+            if (err) {
+              return console.error(err)
+            } else {
+              opportunity.find({}, function (err, opportunities) {
+                if (err) {
+                  return console.error(err)
+                } else {
+                  res.format({
+                    html: function () {
+                      res.render('index', {
+                        'contacts': contacts,
+                        'organisations': organisations,
+                        'opportunities': opportunities
+                      })
                     }
-                });
-              }
-            });
-           });
-        });
-    });
+                  })
+                }
+              })
+            }
+          })
+        }
+      })
+    })
 
-module.exports = router;
+module.exports = router
